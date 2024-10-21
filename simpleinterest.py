76002d666b759dc151118da6999e12a2cbbf28fd
Pallavi1162 <avanipallavi3@gmail.com>
2024-10-21 06:31:16 +0530

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,23 @@
+'''
+You are given principal amount, rate of interest per annum, and time in years. You need to calculate the simple interest.
+Input and Output Format
+Input Format:
+The first line contains the principal amount (principal).
+The second line contains the rate of interest (rate) per annum.
+The third line contains the time (time) in years.
+Output Format:
+A single line containing the simple interest calculated.
+Sample Input 1
+1000
+5
+2
+Sample Output 1
+100.0
+'''
+
+
+principal = float(input())
+rate = float(input())
+time = float(input())
+simple_interest = (principal * rate * time) / 100
+print(simple_interest)
